cac233de9e15e0e2cd7da6c68cb3309cbcf6f2f2
Alejandro Pelozatto <alejandrojpelozatto@gmail.com>
2024-10-19 19:44:15 -0300

    feat: función para agregar stock a cada producto

--- frontend/src/components/Products/ProductList.js
+++ frontend/src/components/Products/ProductList.js
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import axios from '../../config/axiosConfig.js';
 
 const ProductList = ({ page, setPage }) => {
     const [products, setProducts] = useState([]);
     const [totalPages, setTotalPages] = useState(1);
+    const [showStockFields, setShowStockFields] = useState({});
+    const [stockToAdd, setStockToAdd] = useState({});
 
     // Función para eliminar un producto
     const deleteProduct = async (productId, productName) => {
@@ -18,7 +20,7 @@ const ProductList = ({ page, setPage }) => {
     };
 
     // Traer los productos
-    const fetchProducts = async () => {
+    const fetchProducts = useCallback(async () => {
         try {
             const response = await axios.get(`/products?page=${page}`);
             setProducts(response.data.data.docs);
@@ -26,11 +28,11 @@ const ProductList = ({ page, setPage }) => {
         } catch (error) {
             console.error("Error al obtener los productos", error);
         }
-    };
+    }, [page]);
 
     useEffect(() => {
         fetchProducts();
-    }, [page]);
+    }, [fetchProducts]);
 
     const nextPage = () => {
         if (page < totalPages) setPage(prevPage => prevPage + 1);
@@ -40,12 +42,57 @@ const ProductList = ({ page, setPage }) => {
         if (page > 1) setPage(prevPage => prevPage - 1);
     };
 
+    //Función para manejar el click en el botón "Agregar Stock"
+    const toggleStockField = (productId) => {
+        setShowStockFields(prev => ({
+            ...prev,
+            [productId]: !prev[productId]
+        }));
+    }
+
+    const handleStockSubmit = async (productId, e) => {
+        e.preventDefault();
+        const stockAmount = parseInt(stockToAdd[productId], 10) || 0;
+        try {
+            await axios.put(`/products/${productId}/stock`, { stock: stockAmount });
+            alert(`Stock actualizado para el producto con ID: ${productId}`);
+            fetchProducts();
+            setStockToAdd(prev => ({ ...prev, [productId]: "" }));
+        } catch (error) {
+            console.error("Error al actualizar el stock", error);
+        }
+    }
+
+    const handleStockChange  = (productId, e) => {
+        const value = e.target.value;
+        setStockToAdd(prev => ({ ...prev, [productId]: value }));
+    }
+
     return (
         <div>
             <ul>
                 {products.map(product => (
                     <li key={product._id}>
-                        {product.code} - {product.name} - ${product.price} : <button onClick={() => deleteProduct(product._id, product.name)}>Borrar</button>
+                        {product.code} - {product.name} - ${product.price} - Stock: {product.stock} :
+                        <button onClick={() => deleteProduct(product._id, product.name)}>Borrar</button>
+                        <button onClick={() => toggleStockField(product._id)}>
+                            {showStockFields[product._id] ? "Ocultar Stock" : "Agregar Stock"}
+                        </button>
+                        {showStockFields[product._id] && ( // Mostrar campo de entrada solo si es visible
+                            <form className="stockField" style={{ display: 'inline-block' }}  onSubmit={(e) => handleStockSubmit(product._id, e)}>
+                                <div>
+                                    <label htmlFor="stock">Stock:</label>
+                                    <input type="number"
+                                    id="stock"
+                                    name="stock"
+                                    value={stockToAdd[product._id]}
+                                    onChange={(e) => handleStockChange(product._id, e)}
+                                    />
+                                    <button type="submit">OK</button>
+                                </div>
+                                
+                            </form>
+                        )}
                     </li>
                 ))}
             </ul>
